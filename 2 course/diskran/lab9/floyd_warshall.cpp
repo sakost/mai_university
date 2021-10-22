5ab647fa65bec241f3ef1982f88bc33ad6882dd4
VanillaFox <71337244+VanillaFox@users.noreply.github.com>
2021-10-22 22:14:33 +0300

    add if

--- 2 course/diskran/lab9/floyd_warshall.cpp
+++ 2 course/diskran/lab9/floyd_warshall.cpp
@@ -9,7 +9,9 @@ TGraph::AdjMatrix FloydWarshall(const TGraph& graph) noexcept(false){
     for(std::size_t i(0); i < d.size(); ++i){
         for(std::size_t u(0); u < d.size(); ++u){
             for(std::size_t v(0); v < d.size(); ++v){
-                d[u][v] = std::min(d[u][v], d[u][i] + d[i][v]);
+                if(d[u][i] < WEIGHT_MAX && d[i][v] < WEIGHT_MAX){
+                    d[u][v] = std::min(d[u][v], d[u][i] + d[i][v]);
+                }
             }
         }
     }
